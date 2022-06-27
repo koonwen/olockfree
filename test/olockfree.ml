8ed0e103ed6029e32ea2a2678160a5844afaf4c3
koonwen <koonwen@gmail.com>
2022-06-27 21:22:31 +0800

    Add alcotest testing framework

--- test/olockfree.ml
+++ test/olockfree.ml
@@ -20,16 +20,28 @@ let[@warning "-32"] pp_int_option_list = Fmt.list ~sep:Fmt.comma (Fmt.option Fmt
 module T = Domainslib.Task
 
 let test_fun pool =
-  let a1 = T.async pool (fun _ -> enquer q 0 499) in
+  let a1 = T.async pool (fun _ -> enquer q 1 499) in
   let a2 = T.async pool (fun _ -> enquer q 500 1000) in
-  List.iter (fun p -> T.await pool p) [a1;a2]
+  List.iter (fun p -> T.await pool p) [a1;a2];
+  let rec aux n = 
+    match Lockfreeq.deq q with
+    | None -> n
+    | Some _ -> aux (n+1) 
+  in
+  aux 0
+
+let t1 () =
+  let pool = T.setup_pool ~num_additional_domains:2 () in
+  let res = T.run pool (fun _ -> test_fun pool) in
+  T.teardown_pool pool;
+  res
+
+let test_queue_len () =
+  Alcotest.(check int) "No dropped values" 1000 (t1 ())
+
 
 let () =
-  let pool = T.setup_pool ~num_additional_domains:2 () in
-  let _ = T.run pool (fun _ -> test_fun pool) in
-  let res = ref [] in
-  for _ = 0 to 1000 do
-    res := Lockfreeq.deq q :: !res
-  done;
-  Format.printf "%a" pp_int_option_list !res;
-  T.teardown_pool pool;;
+let open Alcotest in
+run "Utils" [
+    "Lockfreeq", [ test_case "Length test" `Quick test_queue_len ];
+  ]
